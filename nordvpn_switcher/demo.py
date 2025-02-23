1e3a02c381847838430729f8f555547a15c20304
MasterCATZ <MasterCATZ@hotmail.com>
2025-02-23 16:52:18 +1000

    Update demo.py
    adding time stamp

--- nordvpn_switcher/demo.py
+++ nordvpn_switcher/demo.py
@@ -1,5 +1,6 @@
 from nordvpn_switcher import initialize_VPN,rotate_VPN
 import time
+import datetime
 
 ##############
 ## WINDOWS ###
@@ -11,8 +12,9 @@ instructions = initialize_VPN() #this will guide you through a step-by-step guid
 
 for i in range(3):
     rotate_VPN(instructions) #refer to the instructions variable here
-    print('\nDo whatever you want here (e.g.scraping). Pausing for 10 seconds...\n')
-    time.sleep(10)
+    now = datetime.datetime.now()
+    print(now.strftime("%Y-%m-%d %H:%M:%S")'\nDo whatever you want here (e.g.scraping). Pausing for 1hour ...\n')
+    time.sleep(3600)
 
 # [2] if you'd like to skip the step-by-step menu (because you want to automate your script fully without any required human intervention, use the area_input parameter
 
